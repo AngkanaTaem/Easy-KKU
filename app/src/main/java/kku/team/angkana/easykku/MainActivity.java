79e7fb7e96ce671d40a010da90fe73fbbd1a6a72
AngkanaTaem <forframeza36@gmail.com>
2016-11-12 12:04:47 +0700

    Intent to SingUp fininsh

--- app/src/main/java/kku/team/angkana/easykku/MainActivity.java
+++ app/src/main/java/kku/team/angkana/easykku/MainActivity.java
@@ -1,13 +1,33 @@
 package kku.team.angkana.easykku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private Button signInButton, signUpButton; //Shift+Ctrl+Enter ->End process
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+        setContentView(R.layout.activity_main); //Ctrl+คลิก วาปหน้า
+
+        //Bind Widget
+        signInButton = (Button) findViewById(R.id.button); //Alt + enter  -> Catch
+        signUpButton = (Button) findViewById(R.id.button2);
+
+        //Sing Up Controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SingUpActivity.class));
+            }
+        });
+
+    } // Main Method
+} // Main Class
